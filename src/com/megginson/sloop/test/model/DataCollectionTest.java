58770de18874bfd0df2f685d60171dc62fb38244
David Megginson <david.megginson@megginson.com>
2013-02-07 19:10:59 -0500

    Update tests to support filters right in the data collection.

--- src/com/megginson/sloop/test/model/DataCollectionTest.java
+++ src/com/megginson/sloop/test/model/DataCollectionTest.java
@@ -3,84 +3,77 @@ package com.megginson.sloop.test.model;
 import com.megginson.sloop.model.DataCollection;
 import com.megginson.sloop.model.DataEntry;
 import com.megginson.sloop.model.DataRecord;
+import com.megginson.sloop.model.ValueFilter;
 
 import junit.framework.TestCase;
 
 public class DataCollectionTest extends TestCase {
 
-	private final static int RECORD_COUNT = 10;
-	private final static int ENTRY_COUNT = 10;
+	private final static String HEADERS[] = { "A", "B", "C", };
+	
+	private final static String VALUES[][] = {
+		{ "A1", "B", "C1" },
+		{ "A2", "D", "C2" },
+		{ "A3", "B", "C3" },
+	};
 
 	private DataCollection mDataCollection;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		
-		// Make the header row
-		String headers[] = new String[ENTRY_COUNT];
-		for (int col = 0; col < ENTRY_COUNT; col++) {
-			headers[col] = makeHeader(col);
-		}
-		
-		mDataCollection = new DataCollection(headers);
-		
-		// Fill in the records
-		for (int row = 0; row < RECORD_COUNT; row++) {
-			String record[] = new String[ENTRY_COUNT];
-			for (int col = 0; col < ENTRY_COUNT; col++) {
-				record[col] = makeValue(row, col);
-			}
-			mDataCollection.addRecord(record);
-		}
-	}
-	
-	public void testCopyConstructor () {
-		DataCollection dataCollection = new DataCollection(mDataCollection);
-		for (int i = 0; i < RECORD_COUNT; i++) {
-			assertEquals(dataCollection.get(i), mDataCollection.get(i));
-			assertNotSame(dataCollection.get(i), mDataCollection.get(i));
+		mDataCollection = new DataCollection(HEADERS);
+		for (String row[] : VALUES) {
+			mDataCollection.addRecord(row);
 		}
 	}
 
 	public void testSize() {
-		assertEquals(RECORD_COUNT, mDataCollection.size());
+		assertEquals(VALUES.length, mDataCollection.size());
 	}
 
 	public void testGet() {
-		for (int row = 0; row < RECORD_COUNT; row++) {
+		for (int row = 0; row < VALUES.length; row++) {
 			DataRecord dataRecord = mDataCollection.get(row);
-			for (int col = 0; col < ENTRY_COUNT; col++) {
+			for (int col = 0; col < HEADERS.length; col++) {
 				DataEntry dataEntry = dataRecord.get(col);
-				assertEquals(makeHeader(col), dataEntry.getKey());
-				assertEquals(makeValue(row, col), dataEntry.getValue());
+				assertEquals(HEADERS[col], dataEntry.getKey());
+				assertEquals(VALUES[row][col], dataEntry.getValue());
 			}
 		}
 	}
-	
+
+	public void testIsFiltered() {
+		assertFalse(mDataCollection.isFiltered());
+		mDataCollection.setFiltered(true);
+		assertTrue(mDataCollection.isFiltered());
+	}
+
+	public void testFilter() {
+		int originalSize = mDataCollection.size();
+		assertNull(mDataCollection.getFilter(HEADERS[1]));
+		mDataCollection.putFilter(HEADERS[1], new ValueFilter() {
+			@Override
+			public boolean isMatch(String value) {
+				return VALUES[0][1].equals(value);
+			}
+		});
+		assertNotNull(mDataCollection.getFilter(HEADERS[1]));
+		
+		// setting a filter doesn't turn on filtering
+		assertEquals(originalSize, mDataCollection.size());
+		assertEquals(originalSize, mDataCollection.sizeUnfiltered());
+		
+		// after turning on filtering, the sizes should be different
+		mDataCollection.setFiltered(true);
+		assertTrue(originalSize > mDataCollection.size());
+		assertEquals(originalSize, mDataCollection.sizeUnfiltered());
+	}
+
 	public void testSearch() {
 		assertEquals(-1, mDataCollection.search("foo", 0));
-		assertEquals(0, mDataCollection.search("0,0", 0));
-		assertEquals(-1, mDataCollection.search("0,0", 1));
-		assertEquals(3, mDataCollection.search("3,1", 0));
-	}
-
-	public void testEquals() {
-		DataCollection dataCollection = new DataCollection(mDataCollection);
-		assertEquals(dataCollection, mDataCollection);
-		assertEquals(mDataCollection, dataCollection);
-	}
-
-	public void testHashCode() {
-		DataCollection dataCollection = new DataCollection(mDataCollection);
-		assertEquals(dataCollection.hashCode(), mDataCollection.hashCode());
-	}
-
-	private String makeHeader(int col) {
-		return "Header " + col;
-	}
-
-	private String makeValue(int row, int col) {
-		return "Value " + row + "," + col;
+		assertEquals(0, mDataCollection.search(VALUES[0][0], 0));
+		assertEquals(-1, mDataCollection.search(VALUES[0][0], 1));
+		assertEquals(2, mDataCollection.search(VALUES[2][2], 0));
 	}
 
 }
